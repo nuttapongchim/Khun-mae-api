b6eb7e06715b68980b3cc30d2c8767b6895ed9b7
BabayakaMan <tanawatnayada@hotmail.com>
2020-07-17 12:12:17 +0700

    gg

--- app.js
+++ app.js
@@ -91,21 +91,31 @@ function FetchUserId(callback) {
    })
 }
 
+FetchToken(function (err,data) {
+    if(err){
+        console.log(err)
+    }else{
+        console.log(data);
+        for(let i =0;i<data.length;i++){
+            console.log(data)
+        }
+    }
+});
 
-// var job = new CronJob('*/5 * * * *', function() {
-//     console.log("gg");
-//     FetchToken(function (err,data) {
-//         if(err){
-//             console.log(err)
-//         }else{
-//             console.log(data);
-//             for(let i =0;i<data.length;i++){
-//                 InsertLogRecord(data[i].MEMBER_ID)
-//             }
-//         }
-//     });
-//
-// }, null, true, 'Asia/Bangkok');
+var job = new CronJob('*/2 * * * *', function() {
+    console.log("gg");
+    FetchToken(function (err,data) {
+        if(err){
+            console.log(err)
+        }else{
+            console.log(data);
+            // for(let i =0;i<data.length;i++){
+            //     InsertLogRecord(data[i].MEMBER_ID)
+            // }
+        }
+    });
+
+}, null, true, 'Asia/Bangkok');
 
 // var SendNotificatonJob = new CronJob('0 13 * * 1',function () {
 //     FetchToken(function (err,data) {
@@ -119,7 +129,7 @@ function FetchUserId(callback) {
 //     });
 // },null,true,'Asia/Bangkok');
 
-// job.start();
+job.start();
 //SendNotificatonJob.start();
 
 const server = app.listen(3003, function () {
